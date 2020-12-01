37c63e28069b55778f4d9d449e0bfd58d9cff894
ALAA999 <alaa07997@gmail.com>
2020-12-01 13:45:26 +0200

    Finish Splash screen

--- app/src/main/java/com/alaa/todolistapp/MainActivity.java
+++ app/src/main/java/com/alaa/todolistapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.alaa.todolistapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.alaa.todolistapp.auth.SignUpActivity;
+import com.alaa.todolistapp.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.next.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.next) {
+            startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/alaa/todolistapp/auth/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.alaa.todolistapp.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alaa.todolistapp.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
