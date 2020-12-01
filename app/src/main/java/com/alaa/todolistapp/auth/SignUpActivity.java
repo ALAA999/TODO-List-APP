919af4835effbdaddd574b143b9b25a2f1169a24
ALAA999 <alaa07997@gmail.com>
2020-12-01 19:39:00 +0200

    finish Sign Up UI

--- app/src/main/java/com/alaa/todolistapp/auth/SignUpActivity.java
+++ app/src/main/java/com/alaa/todolistapp/auth/SignUpActivity.java
@@ -3,14 +3,32 @@ package com.alaa.todolistapp.auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.alaa.todolistapp.R;
+import com.alaa.todolistapp.databinding.ActivityMainBinding;
+import com.alaa.todolistapp.databinding.ActivitySignUpBinding;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.createProfile.setOnClickListener(this);
+        binding.login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.create_profile) {
+
+        } else if (view.getId() == R.id.login) {
+
+        }
     }
 }
